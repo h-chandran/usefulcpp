8d3d9560945c0dcc5aebb2f882ec0e8190779fee
Harish <72057623+h-chandran@users.noreply.github.com>
2024-10-25 19:48:37 -0500

    Add files via upload

---
+++ Binary_Search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+
+    vector<int> v(n);
+    
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    int val; cin >> val;
+    bool found = false;
+    int low = 0, high = n-1;
+    int mid = (low + high) / 2;
+    while(low <= high){
+        if( val == mid){
+            found = true;
+            break;
+        } else if(val > mid){
+            low = mid+1;
+            mid = (low + high) / 2;
+        } else{
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+    }
+    cout << found << endl;
+
+    
+
+    
+    
+    
+}
